36f607d030fb3ac212c0487feb7e757b5c7d3eb1
beksultan <bek2sul2tan@gmail.com>
2022-02-16 10:45:17 +0600

    fest: add 16-02

---
+++ 16-02/script.js
@@ -0,0 +1,12 @@
+let camelCase = 0;
+let snake_case = 1;
+let PascalCase = 2;
+
+alert(camelCase);
+alert(snake_case);
+alert(PascalCase);
+
+let str1 = 'САЛАМ,'
+let str2 = 'БИШКЕК!'
+
+alert(str1 + ' ' + str2)
