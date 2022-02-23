16b04c007399d75cc89f1dece16f8bc6a334963b
beksultan <bek2sul2tan@gmail.com>
2022-02-23 20:23:30 +0600

    fest : add 23-02

---
+++ 23-02/script.js
@@ -0,0 +1,37 @@
+/**
+Создайте объект заработных плат.
+    Назовите userSalaries.
+    Свойствами выступают: Ella, Sophia, Ellie
+Выведите на экран зарплату Ella, Sophia.**/
+
+let userSalaries = {
+    ella: 15500,
+    sophia: 20000,
+    ellie: 30000,
+}
+
+alert(`Зарплата Элли ${userSalaries.ella}`)
+alert(`Зарплата Софии ${userSalaries.sophia}`)
+
+/**
+Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+Найдите сумму его элементов.**/
+let numbers  = {
+    a: 5,
+    b: 6,
+    c: 10,
+}
+alert(numbers.a + numbers.b + numbers.c)
+
+/**
+let number;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}
+
+Сделайте рефакторинг кода, с помощью тернарного оператора**/
+let data = true
+let number = data === true?  3: 5;
+alert(number)
